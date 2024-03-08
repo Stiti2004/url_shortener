60982ccb4326391572a78c01072ad74776e3847a
being_STITI <137472671+Stiti2004@users.noreply.github.com>
2024-03-08 10:40:56 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+//const db = config.get('mongoURI');
+const mongoURI = process.env.mongoURI || config.get('mongoURI');
 
 const connectDB = async () => {
   try {
